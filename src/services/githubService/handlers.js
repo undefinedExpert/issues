d0deeb797b8b236f755dc4a44d8696acc15022d2
Emanuel Slotwinski <ask@eslotwinski.com>
2018-09-17 01:43:13 +0200

    Fix small issue with incorrect date, it should be updated_at instead of created_at

--- src/services/githubService/handlers.js
+++ src/services/githubService/handlers.js
@@ -3,12 +3,12 @@ import { getOrganizationRepositoryList } from './api'
 export const githubIssues = async (organization, repository, since) => {
   const isoDate = since.toISOString()
   const { data } = await getOrganizationRepositoryList(organization, repository, isoDate)
-  return data.map(({user, title, body, url, created_at}) => ({
+  return data.map(({user, title, body, url, updated_at}) => ({
     login: user.login,
     title,
     description: body,
     link: url,
-    date: created_at
+    date: updated_at
   }))
 }
 export const githubReactIssues = async () => {
